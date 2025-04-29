8286de524ee197356ace196060d91b55fdb84d3a
feigonzalez <fei.gonzalezg@duocuc.cl>
2025-04-29 17:18:50 -0400

    Added comments to setup.bat

--- setup.bat
+++ setup.bat
@@ -4,16 +4,24 @@ pip install djangorestframework
 pip install requests
 pip install cx_oracle
 sqlplus
+:: sys as sysdba
+:: system
+:: @prepareDB.sql
+:: exit
 cd gowest
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
-gowest_admin
-gowest_admin@gowest.com
-gowest
-gowest
-y
+:: gowest_admin
+:: gowest_admin@gowest.com
+:: gowest
+:: gowest
+:: y
 cd ..
 sqlplus
+:: gowest_admin
+:: gowest
+:: @popDB.sql
+:: exit
 cd gowest
 python manage.py runserver
